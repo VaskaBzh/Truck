fef89fe1d8cc68f2c93a512a4305e55a38807685
Vasily <90596079+VaskaBzh@users.noreply.github.com>
2021-12-26 22:46:21 +0300

    Add files via upload

---
+++ scroll.js
@@ -0,0 +1,24 @@
+/* const anchor = document.querySelector('.office_img') */
+
+/*for (let anchor of anchors) {
+    anchor.addEventListener("click", function(event) {
+        event.preventDefault();
+        const blockID = anchor.getAttribute('href')
+        document.querySelector('' + blockID).scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        })
+    })
+}   */
+
+/* const top = document.querySelector('.section_europe')
+
+function setScrollIntoViewOptions(top) {
+    const scroll = document.querySelector('.office_img');
+    office_img.scrollIntoView ({
+        behavior: "smooth",
+        block: "start"
+    })
+} */
+
+/* Я очень старался, но не смог разобраться, никакой прокрутки :( */
